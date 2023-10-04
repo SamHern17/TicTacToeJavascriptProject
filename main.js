8a0a042bd79a50b52c10b2c51dbbb4faabc2c197
Sam Hernandez <sam.hernandez.workonly@gmail.com>
2023-10-04 03:23:09 -0400

    updated JS file, functional tic tac toe game, needs fix

--- main.js
+++ main.js
@@ -1,1 +1,88 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.')
+
+/*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+/*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+let win;
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+
+document.getElementById('reset-button').addEventListener('click', init);
+
+/*----- functions -----*/
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    
+    render();
+};
+
+function render() {
+    board.forEach(function(mark, index) {
+    //this sets the text content of the square of the same position to the mark on the board. 
+    squares[index].textContent = mark;
+    });
+    // new code below
+    messages.textContent = `It's ${turn}'s turn!`;
+};
+
+function getWinner() {
+    // just stub it up for now.
+    let winner = null;
+    winningCombos.forEach((combo, index) => {
+    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+    winner = board[combo[0]];
+    }
+    });
+    // new code below
+    if (winner) {
+      return winner 
+    } else if (board.includes('')) {
+      return null // if there's an empty space, return null (no winner yet)
+    } else {
+      return 'T' // no winner and no empty spaces? That's a tie!
+    }
+};
+
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    // This is tidy
+    turn = turn === 'X' ? 'O' : 'X';
+    // In an if statement it would look like: 
+    // if (turn === 'X') {
+    // turn = 'O' 
+    // } else {
+    // turn = 'X' 
+    // };
+    // writing the ternary saved you from all that. 
+    render();
+};
+
+init();
